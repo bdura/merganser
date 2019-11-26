Corrected typos here and there. Notably, the weighting was off in the trajectory node.

packages/merganser_bezier/include/merganser_bezier/bezier.py
@@ -80,7 +80,7 @@ class Bezier(object):
         diff = curve.reshape(-1, 1, 2) - cloud.reshape(1, -1, 2)
         se = (diff ** 2).mean(axis=2)
 
-        return se.min(axis=0).mean() + self.reg * se[[0, 1]].min(axis=1).mean()
+        return se.min(axis=0).mean() + self.reg * se[[0, -1]].min(axis=1).mean()
 
     def create_objective(self, cloud):
 

packages/merganser_bezier/include/merganser_bezier/utils/plots.py
@@ -28,7 +28,7 @@ def plot_fitted_skeleton(beziers, skeletons):
             *to_coordinate(np.vstack((c + .2 * n, c - .2 * n))),
             alpha=.5,
             color='C0',
-            s=10 * np.exp((np.linalg.norm(c + 1 * n, axis=1) - .5) ** 2)
+            s=20 * np.exp(-(np.linalg.norm(c + 1 * n, axis=1) - .5) ** 2)
         )
 
         plt.scatter(*to_coordinate(c_), alpha=.5, color='C' + str(i+1))

packages/merganser_purepursuit/src/merganser_trajectory.py
@@ -74,7 +74,7 @@ class TrajectoryNode(object):
 
         waypoints = np.vstack(points)
 
-        weights = np.exp((np.linalg.norm(waypoints, axis=1, keepdims=True) - self.lookahead))
+        weights = np.exp(-(np.linalg.norm(waypoints, axis=1, keepdims=True) - self.lookahead) ** 2)
         weights /= weights.sum()
 
         waypoint = (waypoints * weights).sum(axis=0)
@@ -88,7 +88,7 @@ class TrajectoryNode(object):
         rospy.loginfo('[%s] %s' % (self.node_name, s))
 
     def on_shutdown(self):
-        rospy.loginfo("[LaneFilterNode] Shutdown.")
+        self.log("Shutting down.")
 
 
 if __name__ == '__main__':