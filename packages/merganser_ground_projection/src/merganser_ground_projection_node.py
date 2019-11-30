549ff125237b378905fefa0d8890fcc941a4bc9b
Tristan Deleu <tristan.deleu@gmail.com>
2019-11-30 16:01:22 -0500

    Refactor ground projection

--- packages/merganser_ground_projection/src/merganser_ground_projection_node.py
+++ packages/merganser_ground_projection/src/merganser_ground_projection_node.py
@@ -1,51 +1,64 @@
 #!/usr/bin/env python
-
-import duckietown_utils as dtu
 import rospy
-from merganser_msgs.msg import SkeletonsMsg
-from merganser_ground_projection.ground_projection_interface import GroundProjection, \
-    get_ground_projection_geometry_for_robot
+import duckietown_utils as dtu
+
+from duckietown_msgs.msg import Vector2D
+
+from merganser_msgs.msg import SkeletonMsg, SkeletonsMsg
+from merganser_ground_projection.utils.factories import get_ground_projection_geometry_for_robot
 
 
 class GroundProjectionNode(object):
-
     def __init__(self):
-        self.node_name = "Merganser Ground Projection"
-        self.active = True
-        # self.bridge = CvBridge()
+        self.node_name = rospy.get_name()
+        self.loginfo('Initializing...')
 
-        robot_name = rospy.get_param("~config_file_name", None)
+        self.robot_name = self.get_robot_name('~veh_name')
+        self.geometry = get_ground_projection_geometry_for_robot(self.robot_name)
 
-        if robot_name is None:
-            robot_name = dtu.get_current_robot_name()
+        # Subscribers
+        self.sub_skeleton = rospy.Subscriber("~skeleton_in",
+                                             SkeletonsMsg,
+                                             self.process_msg)
 
-        self.robot_name = robot_name
+        # Publishers
+        self.pub_skeleton = rospy.Publisher("~skeleton_out",
+                                            SkeletonsMsg,
+                                            queue_size=1)
 
-        self.gpg = get_ground_projection_geometry_for_robot(self.robot_name)
-
-        # Subs and Pubs
-        self.sub_skeleton = rospy.Subscriber("~skeleton_in", SkeletonsMsg, self.process_msg)
-        self.pub_skeleton = rospy.Publisher("~skeleton_out", SkeletonsMsg, queue_size=1)
+        self.loginfo('Initialized')
 
     def process_msg(self, skeletons_msg):
-
+        skeletons = []
         for skeleton in skeletons_msg.skeletons:
-            for p in skeleton.cloud:
+            points = self.geometry.vectors_to_ground(skeleton.cloud)
 
-                # p.x, p.y = 1 - p.x, p.y
-                point = self.gpg.vector2ground(p)
-                # print(point.x, point.x)
+            skeleton_out_msg = SkeletonMsg()
+            skeleton_out_msg.color = skeleton.color
+            skeleton_out_msg.cloud = [Vector2D(x=point.x, y=point.y)
+                                      for point in points]
+            skeletons.append(skeleton_out_msg)
 
-                p.x, p.y = point.x, point.y
-
+        skeletons_out_msg = SkeletonsMsg()
+        skeletons_out_msg.skeletons = skeletons
         self.pub_skeleton.publish(skeletons_msg)
 
-    def onShutdown(self):
-        rospy.loginfo("[MerganserGroundProjectionNode] Shutdown.")
+    def loginfo(self, message):
+        rospy.loginfo('[{0}] {1}'.format(self.node_name, message))
+
+    def get_robot_name(self, name):
+        robot_name = rospy.get_param(name, None)
+        if robot_name is None:
+            robot_name = dtu.get_current_robot_name()
+        rospy.set_param(name, robot_name)
+        return robot_name
+
+    def on_shutdown(self):
+        self.loginfo('Shutting down')
 
 
 if __name__ == '__main__':
-    rospy.init_node('merganser_ground_projection', anonymous=False)
+    rospy.init_node('merganser_ground_projection_node', anonymous=False)
     ground_projection_node = GroundProjectionNode()
-    rospy.on_shutdown(ground_projection_node.onShutdown)
+    rospy.on_shutdown(ground_projection_node.on_shutdown)
     rospy.spin()
