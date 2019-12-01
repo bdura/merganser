11d841ab396d7d737fbd2192fd33945afebc9313
Basile Dura <basile.dura@polytechnique.edu>
2019-12-01 00:09:16 -0500

    Updated the Bezier curve with the Kalman filter.

--- packages/merganser_bezier/include/merganser_bezier/kalman.py
+++ packages/merganser_bezier/include/merganser_bezier/kalman.py
@@ -2,42 +2,74 @@ import numpy as np
 from .utils.kinematics import rotation
 
 from scipy.linalg import block_diag
+from .utils.bernstein import compute_curve, get_bernstein
 
 
 class KalmanFilter(object):
 
-    def __init__(self, mu, process_noise=.1):
+    def __init__(self, dimension=4, process_noise=.1):
 
-        self.mu = mu
-        self.sigma = np.zeros((8, 8))
+        self.dimension = dimension
 
-        self.r = process_noise * np.eye(8)
+        self.mu = np.empty(dimension * 2)
+        self.sigma = np.zeros((dimension * 2, dimension * 2))
+        self.r = process_noise * np.eye(dimension * 2)
+
+    def reset(self, mu):
+        """
+        Resets the covariance matrix to zero (after a gradient descent step for example).
+        """
+
+        self.mu = mu.reshape(-1)
+        self.sigma = np.zeros((self.dimension * 2, self.dimension * 2))
 
     def predict(self, dx, dtheta):
+        r"""
+        Updates the mean and covariance of the estimate based on the control inputs
+        (computes :math:`\bar{bel}(\theta_t)`.
+
+        Parameters
+        ----------
+        dx: float
+            The distance run by the robot in the interval.
+        dtheta: float
+            The rotation performed by the robot in the time step.
+        """
+
         # Constructs the rotation matrix
-        r = block_diag(*[rotation(- dtheta) for _ in range(4)])
+        r = block_diag(*[rotation(- dtheta) for _ in range(self.dimension)])
 
         # Constructs the offset
-        offset = np.zeros(8)
-        offset[list(range(0, 8, 2))] = - dx
+        offset = np.zeros(self.dimension * 2)
+        offset[list(range(0, self.dimension * 2, 2))] = - dx
 
         # Computes the new mean
-        mu_bar = np.matmul(r, self.mu.reshape(-1) + offset).T
+        mu_bar = np.matmul(r, self.mu + offset).T
 
         # Computes the new covariance matrix
         sigma_bar = np.matmul(r, np.matmul(self.sigma, r.T)) + self.r
 
-        self.mu = mu_bar.reshape(4, 2)
+        self.mu = mu_bar
         self.sigma = sigma_bar
 
-    def correct(self, bezier, cloud):
+    def correct(self, cloud):
+        """
+        Corrects the mean and covariance of the estimate using the measurement.
 
-        bezier.controls = self.mu
+        Parameters
+        ----------
+        cloud: np.array
+            The measured cloud point.
+        """
 
-        arg = ((bezier().reshape(1, -1, 2) - cloud.reshape((-1, 1, 2))) ** 2).sum(axis=2).argmin(axis=1)
-        b = bezier.bernstein[arg]
+        bernstein = get_bernstein(order=self.dimension)
 
-        cov = np.cov(np.matmul(b, self.mu).T)
+        curve = np.matmul(bernstein, self.mu.reshape((self.dimension, 2)))
+
+        arg = ((curve.reshape((1, -1, 2)) - cloud.reshape((-1, 1, 2))) ** 2).sum(axis=2).argmin(axis=1)
+        b = bernstein[arg]
+
+        cov = np.cov((curve[arg] - cloud).T)
 
         b = np.array([
             np.hstack([b__ * np.eye(2) for b__ in b_])
@@ -55,12 +87,24 @@ class KalmanFilter(object):
 
         mu = np.matmul(
             sigma,
-            btq_c + np.matmul(sigma_bar_inv, self.mu.reshape(-1))
+            btq_c + np.matmul(sigma_bar_inv, self.mu)
         )
 
-        self.mu = mu.reshape(4, 2)
+        self.mu = mu
         self.sigma = sigma
 
-    def step(self, dx, dtheta, bezier, cloud):
+    def fit(self, dx, dtheta, cloud):
+        """
+        Performs a single step of predict/correct step, using the control inputs and the measured cloud.
+
+        Parameters
+        ----------
+        dx: float
+            The distance run by the robot in the interval.
+        dtheta: float
+            The rotation performed by the robot in the time step.
+        cloud: np.array
+            The measured cloud point.
+        """
         self.predict(dx, dtheta)
-        self.correct(bezier, cloud)
+        self.correct(cloud)
